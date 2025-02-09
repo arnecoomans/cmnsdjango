a79479ec67ca8053021016b80e3905ae4514b7a7
Arne Coomans <app+github@arnecoomans.nl>
2025-02-09 09:46:54 +0100

    simplify configuration

--- context_processors.py
+++ context_processors.py
@@ -28,11 +28,7 @@ def setting_data(request):
     'meta_description': getattr(settings, 'META_DESCRIPTION', 'A CMNS Django Project'),#
     'language_code': getattr(settings, 'LANGUAGE_CODE', 'en'),
 
-    'disallow_delete_attribute': getattr(settings, 'DISALLOW_DELETE_ATTRIBUTE', False),
-    
+    'ajax_load_default': getattr(settings, 'AJAX_LOAD_DEFAULT', default_ajax_load),
     'ajax_load_attributes': getattr(settings, 'AJAX_LOAD_ATTRIBUTES', default_ajax_load),
-    'ajax_load_actionlist': getattr(settings, 'AJAX_LOAD_ACTIONLIST', default_ajax_load),
-    'ajax_load_comments': getattr(settings, 'AJAX_LOAD_COMMENTS', default_ajax_load),
-    'ajax_load_tags': getattr(settings, 'AJAX_LOAD_TAGS', default_ajax_load),
-    'ajax_load_categories': getattr(settings, 'AJAX_LOAD_CATEGORIES', default_ajax_load),
+    'disallow_delete_attribute': getattr(settings, 'DISALLOW_DELETE_ATTRIBUTE', False),
   }
